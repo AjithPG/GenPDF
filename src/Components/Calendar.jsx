4fe0d9a692e8e24a3fd3313468056733db092242
Ajith PG <ajithpg008@gmail.com>
2023-12-25 09:25:05 +0530

    style calendar

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,18 +1,45 @@
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+import "./Calendar.css";
 
 export default function Calendar() {
+  const [lang, setLang] = useState("en");
+
+  const handleLang = (e) => {
+    console.log(e.target.value);
+    setLang(e.target.value);
+  };
+
   return (
     <>
+      <div className="w-full flex justify-center items-center mt-5">
+        <select
+          value={lang}
+          onChange={(e) => handleLang(e)}
+          className="bg-white"
+        >
+          <option value={"en"}>EN</option>
+          <option value={"ar"}>AR</option>
+        </select>
+      </div>
+
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
         events={[
           { title: "event 1", date: "2023-12-01" },
           { title: "event 2", date: "2023-12-02" },
         ]}
-        locale="en"
+        locale={lang}
         direction="rtl"
+        headerToolbar={{
+          left: "next,prev,title",
+          center: "",
+          right: "timeGridDay,timeGridWeek,dayGridMonth",
+        }}
       />
     </>
   );
